8ec54d524e7fb535f3d3a9b68dd598f0825b471a
savvasdalkitsis <kurosavvas@gmail.com>
2026-07-16 11:00:56 +0100

    feat(shelfmark): support external cover rendering for search placeholders

--- packages/types/src/book.ts
+++ packages/types/src/book.ts
@@ -117,6 +117,7 @@ export type BookCard = {
   collapsedSeries?: import("./series-collapse").CollapsedSeriesInfo;
   doesNotExistLocally?: boolean;
   shelfmarkUrl?: string;
+  coverUrl?: string;
 };
 
 export type BookDetailFile = {
